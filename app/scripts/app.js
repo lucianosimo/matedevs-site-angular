e13b09986349bee49feabe02055eee6ca8843783
ViretualBoxImages.com <adminuser@localhost.localdomain>
2016-08-22 21:47:53 -0300

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular
+  .module('matedevsSiteApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl',
+        controllerAs: 'home'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('matedevsSiteApp')
+  .controller('HomeCtrl', function () {
+   
+  });
